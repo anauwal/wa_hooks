f8e8408dbb8352efec53f6be948f8246bc471df2
Boby Kurniawan <boby12kurniawan@gmail.com>
2024-05-21 10:44:44 +0700

    DEV

--- src/version.ts
+++ src/version.ts
@@ -33,7 +33,7 @@ export function getEngineName(): string {
 }
 
 export const VERSION: WAHAEnvironment = {
-  version: '2023.12.3',
+  version: '2024.5.8',
   engine: getEngineName(),
   tier: getWAHAVersion(),
   browser: getBrowserExecutablePath(),
